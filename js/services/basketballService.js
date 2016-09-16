6ee7a0962b50a9f0c24618d8c0a630c1948ae1a4
Carlos Sueldo <thetrip3@live.com>
2016-09-16 17:17:46 -0500

    added controller and service, tested

---
+++ js/services/basketballService.js
@@ -0,0 +1,108 @@
+angular.module('basketballApp')
+    .service('basketballService', function() {
+
+
+
+        this.basketballData = [{
+
+            "name": "Melanie Gordon",
+            "email": "melaniegordon@amtap.com",
+            "phone": "+1 (987) 496-2927"
+        }, {
+
+            "name": "Amber Foreman",
+            "email": "amberforeman@amtap.com",
+            "phone": "+1 (955) 414-2764"
+        }, {
+
+            "name": "Lola Cox",
+            "email": "lolacox@amtap.com",
+            "phone": "+1 (828) 581-2692"
+        }, {
+
+            "name": "Rochelle George",
+            "email": "rochellegeorge@amtap.com",
+            "phone": "+1 (999) 562-2575"
+        }, {
+
+            "name": "Letitia Cameron",
+            "email": "letitiacameron@amtap.com",
+            "phone": "+1 (875) 480-2293"
+        }, {
+            "_id": "57caeff3413e7b27cc0c1ae3",
+            "picture": "http://placehold.it/32x32",
+            "name": "Whitney Adkins",
+            "email": "whitneyadkins@amtap.com",
+            "phone": "+1 (897) 453-3066"
+        }, {
+
+            "name": "Hamilton Curtis",
+            "email": "hamiltoncurtis@amtap.com",
+            "phone": "+1 (990) 505-2927"
+        }, {
+
+            "name": "Nona Ortega",
+            "email": "nonaortega@amtap.com",
+            "phone": "+1 (864) 425-2266"
+        }, {
+
+            "name": "Lea Hahn",
+            "email": "leahahn@amtap.com",
+            "phone": "+1 (993) 501-3734"
+        }, {
+
+            "name": "Edith Dickerson",
+            "email": "edithdickerson@amtap.com",
+            "phone": "+1 (985) 409-2255"
+        }, {
+
+            "name": "Marla Sawyer",
+            "email": "marlasawyer@amtap.com",
+            "phone": "+1 (911) 457-3382"
+        }, {
+
+            "name": "Josefa Mcmillan",
+            "email": "josefamcmillan@amtap.com",
+            "phone": "+1 (895) 409-2972"
+        }, {
+
+            "name": "Tracey Harvey",
+            "email": "traceyharvey@amtap.com",
+            "phone": "+1 (953) 437-3646"
+        }, {
+
+            "name": "Marisol Barr",
+            "email": "marisolbarr@amtap.com",
+            "phone": "+1 (810) 455-3572"
+        }, {
+
+            "name": "Olivia Levy",
+            "email": "olivialevy@amtap.com",
+            "phone": "+1 (981) 584-3451"
+        }, {
+
+            "name": "Johanna Baldwin",
+            "email": "johannabaldwin@amtap.com",
+            "phone": "+1 (800) 547-3377"
+        }, {
+
+            "name": "Baxter Gillespie",
+            "email": "baxtergillespie@amtap.com",
+            "phone": "+1 (822) 512-3231"
+        }, {
+
+            "name": "Karen Stanley",
+            "email": "karenstanley@amtap.com",
+            "phone": "+1 (811) 540-3397"
+        }, {
+
+            "name": "Carver Britt",
+
+            "phone": "+1 (860) 467-2625"
+        }, {
+
+            "name": "Dudley Davenport",
+            "email": "dudleydavenport@amtap.com",
+            "phone": "+1 (989) 540-2141"
+        }];
+    });
