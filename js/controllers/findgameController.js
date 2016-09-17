4ec5d6640ca1911c24ec9e39a75265088b91d77b
Carlos Sueldo <thetrip3@live.com>
2016-09-17 17:44:10 -0500

    added table to findgame.html

--- js/controllers/findgameController.js
+++ js/controllers/findgameController.js
@@ -1,6 +1,7 @@
 angular.module('basketballApp')
   .controller('findgameController', function($scope, basketballService){
 
-    
+    $scope.meetup = basketballService.basketballData;
+    console.log($scope.meetup);
 
   });
